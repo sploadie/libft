4b04a6159cd8f53c446676d655d31ea197c228a1
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2014-11-12 15:40:19 +0100

    Stupid mistake in ft_strnew. Ashamed.

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 09:39:43 by tgauvrit          #+#    #+#             */
-/*   Updated: 2014/11/09 16:52:56 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2014/11/12 15:36:13 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ char	*ft_strnew(size_t size)
 	neoscript = (char *)malloc((size + 1) * sizeof(char));
 	if (!neoscript)
 		return (NULL);
-	ft_bzero(neoscript, size);
+	ft_bzero(neoscript, size + 1);
 	return (neoscript);
 }
