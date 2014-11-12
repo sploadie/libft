a09341d707edae2382a1db3e676f31a61d963c65
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2014-11-12 14:26:40 +0100

    ft_atoi norm fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/01 18:44:23 by tgauvrit          #+#    #+#             */
-/*   Updated: 2014/11/10 18:17:50 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2014/11/12 14:18:53 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,7 @@ int				ft_atoi(char *str)
 	size_t		str_int_len;
 	size_t		i;
 
-	if (str[0] == 0)
-		return (0);
-	str_int_sign = 1;
-	if (str[0] == '-' && str++)
+	if ((str_int_sign = 1) && str[0] == '-' && str++)
 		str_int_sign = -1;
 	else if (str[0] == '+')
 		str++;
@@ -43,8 +40,8 @@ int				ft_atoi(char *str)
 		return (ft_atoi(str + 1));
 	str_int_len = ft_str_int_len(str);
 	str_int = 0;
-	i = 0;
-	while (i < str_int_len)
+	i = -1;
+	while (++i < str_int_len)
 	{
 		str_int = (str_int * 10) + (str[i] - '0');
 		if (str_int < 0)
@@ -53,7 +50,6 @@ int				ft_atoi(char *str)
 				return (0);
 			return (-1);
 		}
-		i++;
 	}
 	return ((int)str_int * str_int_sign);
 }
